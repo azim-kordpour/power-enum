e27bbfd7e66c66bf1b449b174db4ce88e54ffefe
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-02-08 14:31:25 +0100

    Apply rector

--- tests/Unit/Helpers/functionTest.php
+++ tests/Unit/Helpers/functionTest.php
@@ -1,12 +1,12 @@
 <?php
 
-it(description: 'returns string after "is".', closure: function () {
+it(description: 'returns string after "is".', closure: function (): void {
     expect(value: getStringAfterIsForEnumName(string: 'isActive'))
         ->toBeString()
         ->toBe(expected: 'Active');
 });
 
-it(description: 'returns null if the given string does not contain "is".', closure: function () {
+it(description: 'returns null if the given string does not contain "is".', closure: function (): void {
     expect(value: getStringAfterIsForEnumName(string: 'active'))
         ->toBeNull();
 });
--- tests/Unit/Traits/PowerEnumTest.php
+++ tests/Unit/Traits/PowerEnumTest.php
@@ -3,7 +3,7 @@
 use Tests\Unit\Examples\TestComment;
 use Tests\Unit\Examples\TestPost;
 
-test(description: 'The method of "values" works.', closure: function () {
+test(description: 'The method of "values" works.', closure: function (): void {
     expect(value: TestPost::values())
         ->toBeArray()
         ->toBe(expected: [
@@ -12,7 +12,7 @@ test(description: 'The method of "values" works.', closure: function () {
         ]);
 });
 
-test(description: 'The method of "names" works.', closure: function () {
+test(description: 'The method of "names" works.', closure: function (): void {
     expect(value: TestPost::names())
         ->toBeArray()
         ->toBe(expected: [
@@ -21,7 +21,7 @@ test(description: 'The method of "names" works.', closure: function () {
         ]);
 });
 
-test(description: 'The method of "list" works.', closure: function () {
+test(description: 'The method of "list" works.', closure: function (): void {
     expect(value: TestPost::list())
         ->toBeArray()
         ->toBe(expected: [
@@ -30,7 +30,7 @@ test(description: 'The method of "list" works.', closure: function () {
         ]);
 });
 
-test(description: 'The method of "equals" works.', closure: function () {
+test(description: 'The method of "equals" works.', closure: function (): void {
     expect(value: TestPost::Active->equals(value: TestPost::Active))
         ->toBeTrue()
         ->and(value: TestPost::Active->equals(value: TestPost::Active))
@@ -41,7 +41,7 @@ test(description: 'The method of "equals" works.', closure: function () {
         ->toBeFalse();
 });
 
-test(description: 'The method of "is" works.', closure: function () {
+test(description: 'The method of "is" works.', closure: function (): void {
     expect(value: TestPost::Active->is(value: TestPost::Active))
         ->toBeTrue()
         ->and(value: TestPost::Active->is(value: TestPost::Active))
@@ -52,13 +52,13 @@ test(description: 'The method of "is" works.', closure: function () {
         ->toBeFalse();
 });
 
-test(description: 'The method of "setLabels" works.', closure: function () {
+test(description: 'The method of "setLabels" works.', closure: function (): void {
     expect(value: TestPost::setLabels())
         ->toBeArray()
         ->toBeEmpty();
 });
 
-test(description: 'The method of "getLabels" works.', closure: function () {
+test(description: 'The method of "getLabels" works.', closure: function (): void {
     expect(value: TestPost::getLabels())
         ->toBeArray()
         ->toBe(expected: [
@@ -67,57 +67,57 @@ test(description: 'The method of "getLabels" works.', closure: function () {
         ]);
 });
 
-test(description: 'The method of "getLabels" t.', closure: function () {
+test(description: 'The method of "getLabels" t.', closure: function (): void {
     expect(fn () =>  TestComment::getLabels())
     ->toThrow(exception: ErrorException::class, exceptionMessage: "old is not a value of the Enum's case.");
 });
 
-test(description: 'The method of "label" works.', closure: function () {
+test(description: 'The method of "label" works.', closure: function (): void {
     expect(value: TestPost::Active->label())
         ->toBe(expected: 'active')
         ->and(value: TestPost::Inactive->label())
         ->toBe(expected: 'inactive');
 });
 
-test(description: 'The method of "toUpper" works.', closure: function () {
+test(description: 'The method of "toUpper" works.', closure: function (): void {
     expect(value: TestPost::Active->toUpper())
         ->toBe(expected: 'ACTIVE')
         ->and(value: TestPost::Inactive->toUpper())
         ->toBe(expected: 'INACTIVE');
 });
 
-test(description: 'The method of "toLower" works.', closure: function () {
+test(description: 'The method of "toLower" works.', closure: function (): void {
     expect(value: TestPost::Active->toLower())
         ->toBe(expected: 'active')
         ->and(value: TestPost::Inactive->toLower())
         ->toBe(expected: 'inactive');
 });
 
-test(description: 'The method of "toUcFirst" works.', closure: function () {
+test(description: 'The method of "toUcFirst" works.', closure: function (): void {
     expect(value: TestPost::Active->toUcFirst())
         ->toBe(expected: 'Active')
         ->and(value: TestPost::Inactive->toUcFirst())
         ->toBe(expected: 'Inactive');
 });
 
-test(description: 'The method of "fromName" works.', closure: function () {
+test(description: 'The method of "fromName" works.', closure: function (): void {
     expect(value: TestPost::fromName('Active'))
         ->toBe(expected: TestPost::Active);
 });
 
-test(description: 'The method of "fromName" throws exception.', closure: function () {
+test(description: 'The method of "fromName" throws exception.', closure: function (): void {
     expect(fn () => TestPost::fromName('fake'))
     ->toThrow(exception: ErrorException::class, exceptionMessage: 'The given name does not exist.');
 });
 
-test(description: 'The method of "is+Value" works.', closure: function () {
+test(description: 'The method of "is+Value" works.', closure: function (): void {
     expect(value: TestPost::Active->isActive())
         ->toBeTrue()
         ->and(value: TestPost::Active->isInactive())
         ->toBeFalse();
 });
 
-test(description: 'the "__call" throws exception if the method does not exist', closure: function () {
+test(description: 'the "__call" throws exception if the method does not exist', closure: function (): void {
     // @phpstan-ignore-next-line
     expect(fn() =>  TestPost::Active->exists())
     ->toThrow(exception: BadMethodCallException::class, exceptionMessage: "Undefined method 'exists'");
